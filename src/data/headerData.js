4324e3dbe950eca5723ba860f31300fa35b4e477
Spartan <spartan@IMS-119MBA.local>
2022-12-25 17:39:36 -0800

    add photo

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Lohith Raj',
     title: "Software Engineer",
     desciption:"Currently a Graduate Student at San Jose State University with 2+ years of experience as Software Engineer. Have the ability to apply Programming Principles to solve real-world industry problems.",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: '',
     resumePdf: resume
 }
