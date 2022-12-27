176eface50bd377b8c201958bee5a7476ea84a19
Spartan <spartan@IMS-119MBA.local>
2022-12-27 15:20:12 -0800

    GPA added

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,14 +1,14 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'San Jose State University',
+        institution: 'San Jose State University | GPA - 4.0/4.0',
         course: 'Master of Science, Computer Science',
         startYear: '2022',
         endYear: '2024'
     },
     {
         id: 2,
-        institution: 'Bangalore Institute of Technology',
+        institution: 'Bangalore Institute of Technology | GPA - 8.4/10.0',
         course: 'Bachelor of Engineering, Computer Science',
         startYear: '2016',
         endYear: '2020'
