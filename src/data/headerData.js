9d1f074f25ae4e86ee71a62c16ed56323e8aef77
Spartan <spartan@IMS-119MBA.local>
2023-02-01 16:45:10 -0800

    profile picture added

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import lohith from '../assets/png/lohith.jpeg'
 
 export const headerData = {
     name: 'Lohith Raj',
     title: "Software Engineer",
     desciption:"Currently a Graduate Student at San Jose State University with 2+ years of experience as Software Engineer. Have the ability to apply Programming Principles to solve real-world industry problems.",
-    image: '../assets/lohith.jpeg',
+    image: lohith,
     resumePdf: resume
 }
