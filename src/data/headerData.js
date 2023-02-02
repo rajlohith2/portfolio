a4381411c2a64f81cf57d54e1fc2259023a8937e
Lohith Raj <37451524+rajlohith2@users.noreply.github.com>
2023-02-01 16:35:39 -0800

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Lohith Raj',
     title: "Software Engineer",
     desciption:"Currently a Graduate Student at San Jose State University with 2+ years of experience as Software Engineer. Have the ability to apply Programming Principles to solve real-world industry problems.",
-    image: '',
+    image: '../assets/lohith.jpeg',
     resumePdf: resume
 }
