6d4bbe327252ceffb95111914963ef22ec0a8738
Lohith Nagaraja <rajlohith2@gmail.com>
2023-08-10 11:41:38 -0700

    projects updated

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,68 +1,92 @@
-import one from '../assets/svg/projects/one.svg'
-import two from '../assets/svg/projects/two.svg'
-import three from '../assets/svg/projects/three.svg'
-import four from '../assets/svg/projects/four.svg'
-import five from '../assets/svg/projects/five.svg'
-import six from '../assets/svg/projects/six.svg'
-
+import one from "../assets/svg/projects/one.svg";
+import two from "../assets/svg/projects/two.svg";
+import three from "../assets/svg/projects/three.svg";
+import four from "../assets/svg/projects/four.svg";
+import five from "../assets/svg/projects/five.svg";
+import six from "../assets/svg/projects/six.svg";
 
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'E-Commerce App',
-        projectDesc: 'A full stack (MERN) web application (Amazon clone) that allows users to view products and place orders.',
-        tags: ['React', 'Node.js', 'Express.js', 'MongoDB'],
-        code: 'https://github.com/rajlohith2/amazon-clone',
-        demo: 'https://github.com/rajlohith2/amazon-clone',
-        image: one
-    },
-    {
-        id: 2,
-        projectName: 'NextBook',
-        projectDesc: 'This project is a recmomender system that provides book suggestion from 10 million titles.',
-        tags: ['React', 'Flask', 'MongoDB', 'TensorFlow'],
-        code: 'https://github.com/rajlohith2/hackathon-2020',
-        demo: 'https://github.com/rajlohith2/hackathon-2020',
-        image: two
-    },
-    {
-        id: 3,
-        projectName: 'Criminal Surveillance',
-        projectDesc: 'Alerts law enforcement agencies about the location through notifications and text messages when a fugitive is recognized on surveillance cameras.',
-        tags: ['Angular', 'OpenCV', 'Flask','MongoDB'],
-        code: 'https://github.com/rajlohith2/final_year_project_2020',
-        demo: 'https://github.com/rajlohith2/final_year_project_2020',
-        image: three
-    },
-    {
-        id: 4,
-        projectName: 'Livestock Detection and Tracking',
-        projectDesc: 'Helps keeping track of animals in a farm and monitoring them to prevent wandering away.',
-        tags: ['Raspberry Pi', 'Python', 'CNN'],
-        code: 'https://github.com/rajlohith2/YojanaSevaKendra',
-        demo: 'https://github.com/rajlohith2/YojanaSevaKendra',
-        image: four
-    },
-    {
-        id: 5,
-        projectName: 'Yojana Seva Kendra',
-        projectDesc: 'Allows users to check eligibility for various Government Welfare Services and apply in one click.',
-        tags: ['C#', '.NET Framework', 'MySQL'],
-        code: 'https://github.com/rajlohith2/YojanaSevaKendra',
-        demo: 'https://github.com/rajlohith2/YojanaSevaKendra',
-        image: five
-    },
-    {
-        id: 6,
-        projectName: 'Reason For Seasons',
-        projectDesc: 'A graphics project that depicts the position of Earth with respect to the sun during every season',
-        tags: ['OpenGL, C++'],
-        code: 'https://github.com/rajlohith2/reason_for_seasons',
-        demo: 'https://github.com/rajlohith2/reason_for_seasons',
-        image: six
-    },
-]
-
+  {
+    id: 1,
+    projectName: "Chirp - A Twitter Clone",
+    projectDesc:
+      "A full stack (MERN) web application that covers all the features of Twitter.",
+    tags: ["React", "Node.js", "Express.js", "MongoDB"],
+    code: "https://github.com/rajlohith2/",
+    demo: "https://github.com/rajlohith2/",
+    image: one,
+  },
+  {
+    id: 2,
+    projectName: "Blockchain Based Supply Chain Management",
+    projectDesc:
+      "Ethereum smart contracts ensuring transparent tracking and verification of product authenticity.",
+    tags: ["React", "Node.js", "Express.js", "MongoDB"],
+    code: "https://github.com/rajlohith2/supply-chain-using-nfts/",
+    demo: "https://github.com/rajlohith2/supply-chain-using-nfts/",
+    image: one,
+  },
+  {
+    id: 3,
+    projectName: "Stocks Purchase Manager",
+    projectDesc:
+      "An object-oriented Java application to simulate the functioning of a Portfolio Manager.",
+    tags: ["Java", "JavaSwing"],
+    code: "https://github.com/rajlohith2/",
+    demo: "https://github.com/rajlohith2/",
+    image: one,
+  },
+  {
+    id: 4,
+    projectName: "NextBook",
+    projectDesc:
+      "This project is a recmomender system that provides book suggestion from 10 million titles.",
+    tags: ["React", "Flask", "MongoDB", "TensorFlow"],
+    code: "https://github.com/rajlohith2/hackathon-2020",
+    demo: "https://github.com/rajlohith2/hackathon-2020",
+    image: two,
+  },
+  {
+    id: 5,
+    projectName: "Criminal Surveillance",
+    projectDesc:
+      "Alerts law enforcement agencies about the location through notifications and text messages when a fugitive is recognized on surveillance cameras.",
+    tags: ["Angular", "OpenCV", "Flask", "MongoDB"],
+    code: "https://github.com/rajlohith2/final_year_project_2020",
+    demo: "https://github.com/rajlohith2/final_year_project_2020",
+    image: three,
+  },
+  {
+    id: 6,
+    projectName: "Livestock Detection and Tracking",
+    projectDesc:
+      "Helps keeping track of animals in a farm and monitoring them to prevent wandering away.",
+    tags: ["Raspberry Pi", "Python", "CNN"],
+    code: "https://github.com/rajlohith2/YojanaSevaKendra",
+    demo: "https://github.com/rajlohith2/YojanaSevaKendra",
+    image: four,
+  },
+  {
+    id: 7,
+    projectName: "Yojana Seva Kendra",
+    projectDesc:
+      "Allows users to check eligibility for various Government Welfare Services and apply in one click.",
+    tags: ["C#", ".NET Framework", "MySQL"],
+    code: "https://github.com/rajlohith2/YojanaSevaKendra",
+    demo: "https://github.com/rajlohith2/YojanaSevaKendra",
+    image: five,
+  },
+  {
+    id: 8,
+    projectName: "Reason For Seasons",
+    projectDesc:
+      "A graphics project that depicts the position of Earth with respect to the sun during every season",
+    tags: ["OpenGL, C++"],
+    code: "https://github.com/rajlohith2/reason_for_seasons",
+    demo: "https://github.com/rajlohith2/reason_for_seasons",
+    image: six,
+  },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
